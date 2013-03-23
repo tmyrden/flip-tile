ed9fa5098bab43ab73586f82764c985f1d124c91
tmyrden <tmyrden@gmail.com>
2013-03-23 16:13:48 -0300

    jitsu added

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 var http = require("http"),
     socketio = require("socket.io"),
     redis = require("redis"),
-    fs = require("fs"),
-    redisClient = redis.createClient(6379, process.env.IP);
+    fs = require("fs");
+    
+var redisClient = redis.createClient(6379, 'nodejitsudb6390461707.redis.irstack.com');
+redisClient.auth("nodejitsudb6390461707.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4", function(err) {
+  if (err) {
+    throw err;
+  }
+});
+redisClient.on('ready', function () {
+	
+});
 
 
 // create a server
@@ -11,7 +20,7 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     // on every request, we'll output 'Hello world'
     res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(process.env.PORT, process.env.IP, function() { console.log('test')});
+}).listen(8080);
 
 var io = socketio.listen(server);
 
